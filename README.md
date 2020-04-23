18680772067201ff7631d5324d06415bac555c74
Finlay <rossf.seg@gmail.com>
2020-04-23 17:56:24 -0500

    more import.py edits

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Project 1
 
 Web Programming with Python and JavaScript
-# project1_CS50w
+
--- import.py
+++ import.py
@@ -1,9 +1,13 @@
 import csv
 import os
+#import psycopg2
 
-#comment here
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+#DATABASE_URL = os.environ['DATABASE_URL']
+
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
