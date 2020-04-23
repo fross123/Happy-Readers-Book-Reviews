729d98db3f5193aafabd571c007096bace90b406
Finlay <rossf.seg@gmail.com>
2020-04-23 18:04:55 -0500

    things happened. removed header of .csv file

--- import.py
+++ import.py
@@ -1,13 +1,8 @@
 import csv
 import os
-#import psycopg2
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-#DATABASE_URL = os.environ['DATABASE_URL']
-
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
