baa0f3dca140e9ea8f1f2632714777af810ac510
Finlay <rossf.seg@gmail.com>
2020-04-23 18:13:58 -0500

    space out of .csv

--- import.py
+++ import.py
@@ -12,7 +12,7 @@ def main():
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                    {"isbn": isbn, "title": "title", "author": "author", "year": year})
+                    {"isbn": isbn, "title": title, "author": author, "year": year})
         print(f"Added book. ISBN {isbn} title {title} author {author} year {year}.")
     db.commit()
 
