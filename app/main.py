07b106a8d041da2f32e008ad8395409382c4499a
Finlay <rossf.seg@gmail.com>
2020-04-24 18:54:25 -0500

    edit main.py

--- app/main.py
+++ app/main.py
@@ -24,7 +24,3 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
 	return render_template("index.html")
-
-if __name__ == "__main__":
-	port = int(os.enviorn.get("PORT", 8080))
-	app.run(host="0.0.0.0", port=port)
