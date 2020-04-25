1e5f527e6c5d23365d3cf476696a0fe77712ecd2
Finlay <rossf.seg@gmail.com>
2020-04-24 22:28:07 -0500

    try taking out env check for heroku upload

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+#    raise RuntimeError("DATABASE_URL is not set")
 
 #Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -18,7 +18,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
