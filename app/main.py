d3576c85a85d5ca38460809995cfa168750e2d8e
Finlay <rossf.seg@gmail.com>
2020-04-24 22:24:55 -0500

    victory. books.html works.

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 
 # Check for environment variable
-#if not os.getenv("DATABASE_URL"):
-#    raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 #Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -18,7 +18,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine("DATABASE_URL")
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
@@ -28,6 +28,6 @@ def index():
 	
 @app.route("/books")
 def books():
-    """Lists all books."""
-    books = db.execute("SELECT * FROM books LIMIT 10").fetchall()
+    """Lists 20 books."""
+    books = db.execute("SELECT * FROM books LIMIT 20").fetchall()
     return render_template("books.html", books=books)
