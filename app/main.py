27c6463ddd15c02957beadf92b868406f6ad91ee
Finlay <rossf.seg@gmail.com>
2020-04-25 13:39:12 -0500

    start on books page

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import os
 import psycopg2
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-#set ENV
+#set database env
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -27,13 +27,21 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-	return render_template("index.html")
+	books = db.execute("SELECT * FROM books").fetchall()
+	return render_template("index.html", books=books)
+	
+#@app.route("/book/<int:book_id", methods=["POST"])
+#def book_details():
+#	"""Book details"""
+	
+	
 	
 @app.route("/books")
 def books():
     """Lists 20 books."""
     books = db.execute("SELECT * FROM books LIMIT 20").fetchall()
     return render_template("books.html", books=books)
+
+
