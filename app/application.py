208d3431ba11665bcc78a117f9228e0422b864b2
Finlay <rossf.seg@gmail.com>
2020-04-26 11:56:53 -0500

    add login testing

--- app/application.py
+++ app/application.py
@@ -32,4 +32,12 @@ def index():
 	books = db.execute("SELECT * FROM books").fetchall()
 	return render_template("index.html", books=books)
 
-@app.route("review")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	error=None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			return render_template("error.html" message="Invalid Credintials")
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
